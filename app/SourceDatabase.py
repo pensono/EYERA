b85ca132d7028ca1b1d18b67b0a458bd27dc87ed
Elizabeth McKinnie <Elizabeth McKinnie>
2017-10-22 13:01:51 -0700

    create Source to store name, reliability, and polarization. Create SourceDatabase to store multiple sources and access certain polarization and the most opposite (based on polarization). Example client (extract_polarization) creates sources and database and finds two sources most opposite.

---
+++ app/SourceDatabase.py
@@ -0,0 +1,37 @@
+import math
+
+
+class SourceDatabase(object):
+    sources = []
+
+    def __init__(self, source):
+        self.sources.append(source)
+
+    def get_polarization(self, name):
+        for x in range(0, len(self.sources)):
+            if self.sources[x].name == name:
+                return self.sources[x].polarization
+
+    #returns tuple of most opposite sources; highest (most right) source first and lowest (most left) sources second.
+    def find_most_different(self):
+        return self.find_max(), self.find_min()
+
+    def find_max(self):
+        max = self.sources[0]
+        for x in range(1, len(self.sources)):
+            if self.sources[x].polarization > max.polarization:
+                max = self.sources[x]
+        max.highest = True
+        return max
+
+    def find_min(self):
+        min = self.sources[0]
+        for x in range (1, len(self.sources)):
+            if self.sources[x].polarization < min.polarization:
+                min = self.sources[x]
+        min.lowest = True
+        return min
+
+
+
+
