cd61dd9b3048f875ed41e17fe54c7c2e4abd9614
Yael <ygoldin@hotmail.com>
2017-10-22 14:55:01 -0700

    related articles

--- highlight.js
+++ highlight.js
@@ -10,6 +10,13 @@ chrome.tabs.executeScript( {
       data: {"highlight": selection[0] },
       success: function(result){
         var article = result.article;
+        var recommended = result.recommended;
+
+        for(var i = 0; i < recommended.length; i++){
+            var oneRec = recommended[i];
+            $("#ajax").append("<p><a target=\"_blank\" href=\"" + oneRec.url + "\">" + oneRec.title + "</a></p>");
+        }
+
         var paragraphs = article.split("\n");
         for(var i = 0; i < paragraphs.length; i++){
             var par = paragraphs[i];
