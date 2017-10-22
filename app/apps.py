27c5ccf5c85bf16901b5e3acb461f945e5d66661
Ethan Shea <ethan.shea1@gmail.com>
2017-10-22 04:26:00 -0700

    Rename stuff. frankenarticle -> app
    I have changed my mind.

--- app/apps.py
+++ app/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class FrankenarticleConfig(AppConfig):
-    name = 'frankenarticle'
+    name = 'app'
--- eyera/urls.py
+++ eyera/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^frankenarticle/', include('frankenarticle.urls')),
+    url(r'^app/', include('app.urls')),
 ]
