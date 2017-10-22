e02b160bc7ec2504fd90a383de0e7a6c56b98d16
Yael <ygoldin@hotmail.com>
2017-10-22 08:47:53 -0700

    added jquery and made related changes to try to make ajax work

--- highlight.js
+++ highlight.js
@@ -3,14 +3,3 @@ chrome.tabs.executeScript( {
 }, function(selection) {
   document.getElementById("output").innerHTML = selection[0];
 });
-
-/*$(document).ready(function(){
-  $("button").click(function(){
-    $.ajax({
-      url: 'http://localhost:8000/app/',
-      success: function(result){
-        $("#output").html(result);
-      }
-    });
-  });
-}); SHOULD WORK FOR GETTING AJAX CALL BUT DOESNT???*/
