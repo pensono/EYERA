52c0da9b10e54f68cdfd170c99ee3c58ce5d4fa7
Ethan Shea <ethan.shea1@gmail.com>
2017-10-25 18:25:20 -0700

    Rename eyera -> Factually

--- factually/wsgi.py
+++ factually/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for eyera project.
+WSGI config for factually project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eyera.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "factually.settings")
 
 application = get_wsgi_application()
